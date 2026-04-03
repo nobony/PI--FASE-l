a313004b12efb90b4a09b84645c6e70438578246
Sofia Carvalho <sofia.junho@gmail.com>
2026-04-03 19:26:48 -0300

    produto.c preenchido com a manipulacao do vetor

--- src/produto.c
+++ src/produto.c
@@ -1,16 +1,34 @@
-#include <stdio.h>
+#include <stdlib.h>
 #include "produto.h"
 
 //manipulacao do vetor
-/* -inicializacao
-    -insercao com realloc
+/* -inicializacao (malloc porque comeca com 0)
+    -insercao com realloc (para continuar de onde parou)
     -liberacao de memoria*/
 
-//parseamento dos dados com srctrok sla
-Produto* parseProduto(char* linha){
-    
+//iniciar
+void inicVetor (VetorProdutos *v){
+    v->tamanho = 0;
+    v->capacidade = 1000;
+    v->dados = malloc(v->capacidade * sizeof(Produto));
 }
 
 
-//armazenamento
-void adicionarProduto();
+//inserir
+void addProduto(VetorProdutos *v, Produto p){
+    //quando o vetor está cheio:
+    if(v->tamanho == v->capacidade){
+        v->capacidade *= 2; //dobra a capacidade
+        v->dados = (Produto*) realloc(v->dados, v-> capacidade * sizeof(Produto));//realocou a memória
+    }
+    v->dados[v->tamanho++] = p;
+}
+
+
+//liberar para evitar problemas
+void liberarMemoria(VetorProdutos *v){
+    if(v->dados != NULL){
+        free(v->dados);
+        v->dados = NULL;
+    }
+}
