4d29f23f8dd8887ca6da5d8e7a45891bcfa27b7d
Sofia Carvalho <sofia.junho@gmail.com>
2026-04-03 18:16:23 -0300

    produto.h preenchido com a struct e as sintaxes das funcoes

--- src/produto.h
+++ src/produto.h
@@ -1,16 +1,28 @@
-#include <stdio.h>
+#ifndef PRODUTO_H
+#define PRODUTO_H
 
 //struct do produto (id,nome,categoria,valor)
-struct Produto {
+typedef struct {
     long int id;
     char nome[50];
     char categoria [50];
     float valor;
-};
+} Produto;
 
 //struct VetorProdutos
-struct VetorProdutos{
+typedef struct{
     struct Produto* dados;
     int tamanho;
     int capacidade;
-};
+} VetorProdutos;
+
+//sintaxe funcao inicializarVetor
+void inicVetor(VetorProdutos *c); 
+
+//sintaxe funcao addProduto
+void addProdutos(VetorProdutos *c, Produto p);
+
+//sintaxe funcao liberarMemoria
+void liberarMemoria(VetorProdutos *v);
+
+#endif 
