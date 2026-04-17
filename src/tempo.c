0d8ee9e7dcaf43088bb7839c9ea3e3619a0d1613
Victor Sousa <vpereira@iesb.edu.br>
2026-04-17 02:42:26 -0300

    Finalizando funcao do tempo

--- src/tempo.c
+++ src/tempo.c
@@ -1,4 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 
-//executar e medir os testes
-// funcao clock_t
+// Esta é a sua ferramenta de medição
+double obterTempo() {
+    return ((double)clock() / CLOCKS_PER_SEC) * 1000.0;
+}
+
+// Exemplo de como você usaria para medir 1000 buscas no FIM
+double t_inicio = obterTempo(); // Foto 1
+
+for (int i = v.tamanho - 1000; i < v.tamanho; i++) {
+    buscaSequencial(&v, v.dados[i].id);
+}
+
+double t_fim = obterTempo(); // Foto 2
+
+printf("Tempo total para as 1000 buscas no fim: %.4f ms\n", t_fim - t_inicio);
